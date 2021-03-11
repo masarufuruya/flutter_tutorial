2ea2eeb7f930255aaa71409784c50017f6d712c7
masarufuruya <masarufuruya116@gmail.com>
2021-03-11 23:48:09 +0900

    Add input unit form

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'unit.dart';
 import 'category.dart';
+import 'unit.dart';
+
+const _padding = EdgeInsets.all(16.0);
 
 class ConverterRoute extends StatefulWidget {
   @override
@@ -8,6 +10,55 @@ class ConverterRoute extends StatefulWidget {
 }
 
 class _ConverterRouteState extends State<ConverterRoute> {
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue;
+  String _convertedValue = '';
+  bool _showValidationError = false;
+
+  /// Clean up conversion; trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
+
+  void _updateConversion() {
+    setState(() {
+      _convertedValue =
+          _format(_inputValue * (_toValue.conversion / _fromValue.conversion));
+    });
+  }
+
+  void _updateInputValue(String input) {
+    setState(() {
+      if (input == null || input.isEmpty) {
+        _convertedValue = '';
+      } else {
+        // Even though we are using the numerical keyboard, we still have to check
+        // for non-numerical input such as '5..0' or '6 -3'
+        try {
+          final inputDouble = double.parse(input);
+          _showValidationError = false;
+          _inputValue = inputDouble;
+          _updateConversion();
+        } on Exception catch (e) {
+          print('Error: $e');
+          _showValidationError = true;
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final CoverArguments args = ModalRoute.of(context).settings.arguments;
@@ -18,30 +69,25 @@ class _ConverterRouteState extends State<ConverterRoute> {
         title: Text(args.categoryName,
             style: TextStyle(fontSize: 32, color: Colors.black54)));
 
-    final unitWidgets = args.units.map((Unit unit) {
-      return Container(
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
-            ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    }).toList();
+    final input = Padding(
+        padding: _padding,
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          TextField(
+            style: Theme.of(context).textTheme.headline4,
+            decoration: InputDecoration(
+                labelStyle: Theme.of(context).textTheme.headline4,
+                errorText:
+                    _showValidationError ? 'Invalid number entered' : null,
+                labelText: 'Input',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(0.0))),
+            keyboardType: TextInputType.number,
+            onChanged: _updateInputValue,
+          )
+        ]));
 
     return Scaffold(
-        appBar: appBar,
-        backgroundColor: args.categoryColor,
-        body: ListView(
-          children: unitWidgets,
-        ));
+        appBar: appBar, backgroundColor: args.categoryColor, body: input);
   }
 }
